c33bebe138be20f6b255226ed05cee32a1dd8bf7
TarasKarabas <taraskyparenko@gmail.com>
2023-03-30 17:30:02 +1100

    Initial Commit

---
+++ Convertor/ConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertorApp.swift
+//  Convertor
+//
+//  Created by Taras Kyparenko on 30/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct ConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
